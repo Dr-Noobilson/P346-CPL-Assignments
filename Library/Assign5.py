63ab52e2ef4f0fd04cdc21866db0b45a383259ef
Dekks-Zelphics <deependra.singh@niser.ac.in>
2022-11-16 04:15:14 +0530

    Assignment 5

--- Library/Assign5.py
+++ Library/Assign5.py
@@ -14,7 +14,6 @@ def intMid(funx,a,b,N):
         sum= sum + (h*funx(a+((i+0.5)*h)))
         
     sum=round(sum,8)
-    print("sum for N (",N,") =",sum)
     return sum
     
     
@@ -32,13 +31,12 @@ def intTrap(funx,a,b,N):
         sum= sum + k*funx(a+i*h)
     
     sum=round(sum*h/2, 8)
-    print("sum for N (",N,") =",sum)
     return sum
 
 
 
 #Simpson
-def Simp(funx,a,b,N):
+def simp(funx,a,b,N):
     k=1
     sum=0
     h=(b-a)/N
@@ -50,7 +48,55 @@ def Simp(funx,a,b,N):
         sum= sum + k*funx(a+i*h)
 
     sum=round(sum*h/3, 8)
-    print("sum for N (",N,") =",sum)
     return sum
 
+
+#Function to print all three in table form
+def numInt(funx,a,b,N):
+    print(N,"   ",intMid(funx,a,b,N),"   ",intTrap(funx,a,b,N),"   ",simp(funx,a,b,N))
+    
+ 
+
+#Random number generator between two numbers a and b
+j=32768
+
+def LCG(n,a,b,k):
+  x=[]
+  for i in range(n):
+    k=rand(k)/j
+    x.append(a+(b-a)*k)
+  return x
+
+
+#Monte Carlo  
+def monteC(funx,a,b,N,e):
+    
+    sum1=sum2=0
+    r=LCG(N,a,b,e)
+    
+    for i in range(N):
+        sum1+=funx(r[i])**2
+        sum2+=funx(r[i])
+    
+    sum1= sum1/N + (sum2/N)**2
+    sum2=(b-a)*sum2/N
+    
+    return sum2,math.sqrt(sum1)
+    
+
+#Function to plot the convergence of integral using Monte Carlo for different N   
+def mcPlot(funx,a,b,N,y):
+    p=[]
+    q=[]
+    r=10
+    for i in range(N+1):
+        p.append(r)
+        q.append(monteC(funx,a,b,r,0.1)[0])
+        r=int((i+1)*10000)
+        
+    f=plt.figure()
+    plt.xlabel("N")
+    plt.ylabel("Fn")   
+    plt.plot(p,q)   
+    plt.plot(np.arange(10,r*1.5,(r*1.5-10)/3),[y,y,y]) 
 
